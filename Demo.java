fdc0f27c729231cbaea3cb799a9714f01da4043b
nayana080 <160204999+nayana080@users.noreply.github.com>
2024-02-16 10:22:40 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+	public static void main(String args[]) {
+		System.out.println("Today we are learning Jenkin CI and CD tools");
+		System.out.println("We are creating more than one jenkin jobs");
+	}
+}
